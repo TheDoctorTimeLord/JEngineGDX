0dc9924af8519f5bcf629057a0985990ea847920
TheDoctorTimeLord <fedzam99@mail.ru>
2023-07-08 23:32:02 +0500

    start for research

---
+++ desktop/src/ru/jengine/jenginegdx/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.jengine.jenginegdx;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.jengine.jenginegdx.ApplicationController;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("JEngineGDX");
+		new Lwjgl3Application(new ApplicationController(), config);
+	}
+}
--- src/main/java/ru/jending/TestGdx.java
+++
@@ -1,9 +0,0 @@
-package ru.jending;
-
-import com.badlogic.gdx.Gdx;
-
-public class TestGdx {
-    public void test() {
-        Gdx.app.error("JEngineGDX", "Providing LibGdx test is successful");
-    }
-}
